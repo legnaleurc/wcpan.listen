9c11e92ec5db588fc181543ad723b6eef0741db1
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-19 06:33:21 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='wcpan.listen',
-    version='1.0.1',
+    version='1.0.2',
     author='Wei-Cheng Pan',
     author_email='legnaleurc@gmail.com',
     url='https://github.com/legnaleurc/wcpan.listen',
